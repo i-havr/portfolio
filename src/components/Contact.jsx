516c6949baa848f6a8ed722136ab7f8d3cf506d1
i-havr <i.havrylov@gmail.com>
2023-05-09 01:08:37 +0300

    edit email settings

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,10 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_aqkzd18",
+        "template_wtid3ys",
+        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Ihor Havrylov",
@@ -42,7 +44,8 @@ const Contact = () => {
           to_email: "i.havrylov@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "zU5PP6RXpMczaJ7Zi"
+        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
